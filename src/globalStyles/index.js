4729becf2e9fe7163d885324ef502921008f5f49
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-02 15:54:20 +0200

    add routes

---
+++ src/globalStyles/index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+    box-sizing:border-box;
+    padding:0;
+    margin:0;
+    
+}
+body{
+  background-color: #f3f2ef;
+
+}
+
+`;
+
+export default GlobalStyles;
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+export const routes = {
+  home: "/",
+  dog: "/dogs",
+  cat: "/cats",
+  genralClinic: "/genral_clinic",
+  contact: "/contact",
+};
