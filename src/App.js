c9bfc20057e0a53f7e2ef4cfa87d42dc7c9e1d3f
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-05-30 15:11:11 +0200

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 /** @format */
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AppWrapper } from "./AppStyles";
 import Navbar from "./components/navbar/Navbar";
 import GlobalStyles from "./globalStyles";
+import { routes } from "./routes";
+import Home from "./views/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Navbar />
+      <Switch>
+        <AppWrapper>
+          <Route exact path={routes.home} component={Home}></Route>
+        </AppWrapper>
+      </Switch>
     </BrowserRouter>
   );
 }
