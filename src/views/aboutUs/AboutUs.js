16010342e12e5ee3eb3b5507610d77ce3bf08f11
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-06-04 19:04:41 +0200

    add section aboutus

---
+++ src/views/aboutUs/AboutUs.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import React from "react";
+import InfoColumn from "../../components/infoColumn/InfoColumn";
+import { AboutUsWrapper } from "./AboutUsStyles";
+
+const AboutUs = () => {
+  return (
+    <AboutUsWrapper>
+      <InfoColumn
+        heading="Nasza misja"
+        description="Nieprzerwanie od 1993 roku niesiemy pomoc potrzebującym zwierzętom. Całą dobę, siedem dni w tygodniu. Każdy z naszych pacjentów jest czyimś przyjacielem i właśnie tak go traktujemy – jak przyjaciela.
+        Wszystkie oferowane zabiegi przeprowadzamy z najwyższą troską o bezpieczeństwo oraz spokój zwierzaków, korzystając wyłącznie z profesjonalnych metod i nowoczesnych urządzeń. Nasze indywidualne podejście do pacjentów zostało wielokrotnie docenione w postaci licznych nagród oraz wyróżnień w Polsce i za granicą. Kierujemy się wytycznymi i standardami przyjętymi jako wzory postępowania klinicznego w renomowanych specjalistycznych organizacjach – zarówno europejskich, jak i światowych."
+      />
+      <InfoColumn
+        heading="Nasza misja"
+        description="Nieprzerwanie od 1993 roku niesiemy pomoc potrzebującym zwierzętom. Całą dobę, siedem dni w tygodniu. Każdy z naszych pacjentów jest czyimś przyjacielem i właśnie tak go traktujemy – jak przyjaciela.
+        Wszystkie oferowane zabiegi przeprowadzamy z najwyższą troską o bezpieczeństwo oraz spokój zwierzaków, korzystając wyłącznie z profesjonalnych metod i nowoczesnych urządzeń. Nasze indywidualne podejście do pacjentów zostało wielokrotnie docenione w postaci licznych nagród oraz wyróżnień w Polsce i za granicą. Kierujemy się wytycznymi i standardami przyjętymi jako wzory postępowania klinicznego w renomowanych specjalistycznych organizacjach – zarówno europejskich, jak i światowych."
+      />
+      <InfoColumn
+        heading="Nasza misja"
+        description="Nieprzerwanie od 1993 roku niesiemy pomoc potrzebującym zwierzętom. Całą dobę, siedem dni w tygodniu. Każdy z naszych pacjentów jest czyimś przyjacielem i właśnie tak go traktujemy – jak przyjaciela.
+        Wszystkie oferowane zabiegi przeprowadzamy z najwyższą troską o bezpieczeństwo oraz spokój zwierzaków, korzystając wyłącznie z profesjonalnych metod i nowoczesnych urządzeń. Nasze indywidualne podejście do pacjentów zostało wielokrotnie docenione w postaci licznych nagród oraz wyróżnień w Polsce i za granicą. Kierujemy się wytycznymi i standardami przyjętymi jako wzory postępowania klinicznego w renomowanych specjalistycznych organizacjach – zarówno europejskich, jak i światowych."
+      />
+    </AboutUsWrapper>
+  );
+};
+
+export default AboutUs;
