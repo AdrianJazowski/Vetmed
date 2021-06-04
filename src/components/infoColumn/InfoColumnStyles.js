35856fc49a022cfedb594992d085b5fc9bbc3a58
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-06-04 19:04:27 +0200

    add first styles for infocolumn component

---
+++ src/components/infoColumn/InfoColumnStyles.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import styled from "styled-components";
+
+export const SingleArticle = styled.article`
+  margin: 0 0.5rem 0 0.5rem;
+  padding: 0 0.5rem 0 0.5rem;
+  h1 {
+    color: rgba(25, 139, 202, 0.7);
+    font-size: 2rem;
+    margin-bottom: 0.5rem;
+  }
+  p {
+  }
+`;
