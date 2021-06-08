ea5fbf2a62278c7e666977e90f0d226946b6ebd7
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-06-08 15:13:31 +0200

    add styles for component

---
+++ src/views/aboutUs/AboutUsStyles.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import styled from "styled-components";
+
+export const AboutUsWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  margin: 1rem;
+  width: 80%;
+  margin: auto;
+  transform: translate(0, -100px);
+`;
